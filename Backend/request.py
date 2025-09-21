b1686dc1986a6c66070e998e0b48ec9bab484118
Prince <144636788+PrinceZ-8@users.noreply.github.com>
2025-09-20 21:35:50 -0700

    request.py

--- Backend/request.py
+++ Backend/request.py
@@ -9,9 +9,11 @@ SUPER_MEMORY_KEY = ""
 
 #Google Gemini Request ----------------------------------------------
 def get_icebreakers():
+    audio.record_audio()
+    text = audio.audio_to_text()
     # sample_text = "Hello my name is prince, I am majoring in computer science, I take computer science"
     prefix_question = "Form exactly 5 Icebreaker questions, based the following user conversation, to continue the conversation."
-    content = prefix_question + audio.text
+    content = prefix_question + text
 
     client = genai.Client(api_key=GEMINI_API_KEY)
 
