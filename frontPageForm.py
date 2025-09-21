fa967c44de940f2bad6cbdd59794a65830adc53c
ethanngyen <ahn008huy@gmail.com>
2025-09-20 20:19:18 -0700

    Update frontPageForm.py
    Removed the initial "frontPageForm" and turned the "mainUI" into "frontPageForm"

--- frontPageForm.py
+++ frontPageForm.py
@@ -1,4 +1,11 @@
 import flet as ft
+from flet import View, Page, AppBar, ElevatedButton, Text
+from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
+import homeScreen
+
+#List of users
+usernameList = ["enguyen", "daher", "psolomon"]
+passwordList = ["ethan2007", "dhruva2007", "prince2007"]
 
 #Keep this global to reference in the function
 username_field = ft.TextField(
@@ -25,64 +32,107 @@ password_field = ft.TextField(
     can_reveal_password = True,
 )
 
-def form_submit_function(e):
-    print("Form Submitted")
-    
-    #List of users
-    usernameList = ["enguyen", "daher", "psolomon"]
-    passwordList = ["ethan2007", "dhruva2007", "prince2007"]
-
-    #Checking if the user input is in the username database
-    if (username_field.value in usernameList):
-
-        #Get the index
-        index = usernameList.index(username_field.value)
-
-        if (password_field.value in passwordList and 
-            passwordList[index] == password_field.value):
-            
-            #Login successful
-            print("Login Unsuccessful")
-            print(f"Username: {username_field.value}")
-            print(f"Password: {password_field.value}")
-    else:
-        print("Login Unsuccessful")
-
-form_container = ft.Container(
-    ft.Column([
-        ft.Text("Welcome to our Icebreaker App", size = 24, color = 'black', weight = 'w600'),
-        username_field,
-        password_field,
-        ft.Button(
-            "Submit", 
-            on_click = form_submit_function,
-            color = 'white',
-            width = 700,
-            height = 40,
-            style = ft.ButtonStyle(
-                text_style = ft.TextStyle(size = 16, weight = 'w600'),
-                bgcolor = '#87CEEB',
-            )
-        )
-        
-
-    ], horizontal_alignment= 'center'),
-    width = 400, 
-    height = 400, 
-    bgcolor = 'white',
-    border_radius = 18,
-    padding = 20,
-)
-
-def main(page:ft.Page):
-    
-    page.window.width = 600
-    page.window.height = 500
+def main(page: Page) -> None:
+    page.window.width = 800
+    page.window.height = 700
     page.bgcolor='#87CEEB'
     page.vertical_alignment = 'center'
     page.horizontal_alignment = 'center'
-    page.add(
-        form_container
-    )
 
-ft.app(main)
+    def form_submit_function(e):
+
+        #Checking if the user input is in the username database
+        if (username_field.value in usernameList):
+
+            #Get the index
+            index = usernameList.index(username_field.value)
+
+            if (password_field.value in passwordList and 
+                passwordList[index] == password_field.value):
+                
+                #Login successful
+                print("Login Successful")
+                print(f"Username: {username_field.value}")
+                print(f"Password: {password_field.value}")
+                page.go('/mainUI')
+        else:
+            print("Login Unsuccessful")
+
+    def route_change(e: RouteChangeEvent) -> None:
+        page.views.clear()
+
+        #Login View
+        page.views.append(
+            View(
+                route='/',
+                vertical_alignment=MainAxisAlignment.CENTER,
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+                bgcolor = '#87CEEB',
+                controls = [
+                    ft.Container(
+                    ft.Column([
+                        ft.Text("Welcome to our Icebreaker App", size = 24, color = 'black', weight = 'w600'),
+                        username_field,
+                        password_field,
+                        ElevatedButton(text="Go to Main UI", 
+                                        on_click=form_submit_function,
+                                        color = 'white',
+                                        width = 700,
+                                        height = 40,
+                                        style = ft.ButtonStyle(
+                                        text_style = ft.TextStyle(size = 16, weight = 'w600'),
+                                        bgcolor = '#87CEEB',
+                                        )
+                                      )
+                    ], horizontal_alignment = 'center'),
+                    width = 400, 
+                    height = 400, 
+                    bgcolor = 'white',
+                    border_radius = 18,
+                    padding = 20,
+                    )
+        ]))
+
+        #Main UI
+        if page.route == '/mainUI':
+            page.views.append(
+            View(
+                route='/',
+                vertical_alignment=MainAxisAlignment.CENTER,
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+                spacing=26,
+                
+                controls=[ ft.Column(
+                    expand=True,   # fill available space
+                    controls=[
+                        homeScreen.instruction_text,
+                        homeScreen.main_row,
+                        ft.Container(expand=True),  # pushes the button down
+                        ft.Row(
+                            controls=[
+                                ft.ElevatedButton(
+                                    text="Go back",
+                                    on_click=lambda _: page.go('/'),
+                                    width=120,
+                                )
+                            ],
+                            alignment=ft.MainAxisAlignment.START,  # left
+                        )
+                    ]
+                )]
+            ))
+                
+        page.update()
+
+    def view_pop(e: ViewPopEvent) -> None:
+        page.views.pop()
+        if page.views:
+            top_view: View = page.views[-1]
+            page.go(top_view.route)
+    
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+if __name__ == '__main__':
+    ft.app(target=main)
