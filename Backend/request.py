fc9186998010881a5d28596b4848a1e35871cede
ethanngyen <ahn008huy@gmail.com>
2025-09-21 11:50:07 -0700

    Update request.py

--- Backend/request.py
+++ Backend/request.py
@@ -1,11 +1,13 @@
 from google import genai
 from supermemory import Supermemory
 #import audio
-#import os
+import os
+from dotenv import load_dotenv
+from frontPageForm import userNamePassword
 
 
-GEMINI_API_KEY = ""
-SUPER_MEMORY_KEY = ""
+# Load environment variables
+load_dotenv()
 
 #Google Gemini Request ----------------------------------------------
 def get_icebreakers(text):
@@ -15,7 +17,13 @@ def get_icebreakers(text):
     prefix_question = "Form exactly 5 Icebreaker questions, based the following user conversation, to continue the conversation."
     content = prefix_question + text
 
-    client = genai.Client(api_key=GEMINI_API_KEY)
+    client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"), 
+                          http_options={"base_url": "https://api.supermemory.ai/v3/https://generativelanguage.googleapis.com", 
+                                        "api_version": "v1", 
+                                        "headers": {
+                                            "x-supermemory-api-key": os.environ.get("SUPERMEMORY_API_KEY"),
+                                            'x-sm-user-id': userNamePassword["enguyen"]
+                                        }})
 
     response = client.models.generate_content(
 	    model="gemini-2.5-flash",
@@ -27,7 +35,8 @@ def get_icebreakers(text):
 
     #Supermemory Storage -------------------------------------------
 
-    sm_client = Supermemory(api_key=SUPER_MEMORY_KEY, base_url="https://api.supermemory.ai/")
+    sm_client = Supermemory(api_key=os.environ.get("SUPERMEMORY_API_KEY"), 
+                            base_url="https://api.supermemory.ai/")
     training_info = content + generated_text
     store_response = sm_client.memories.add(content=training_info)
 
