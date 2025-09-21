68944e133b95f4b1e72731f1a35eacf8ce163ff5
ethanngyen <ahn008huy@gmail.com>
2025-09-20 22:29:35 -0700

    Update homeScreen.py
    Replaced "refresh" button with "cancel" button. Replaced images with those that are free to use.

--- homeScreen.py
+++ homeScreen.py
@@ -1,7 +1,8 @@
 import flet as ft
+import buttonClick
 
 instruction_text = ft.TextField(
-    value = "Please press on the microphone and speak into it so it can suggest an icebreaker topic.",
+    value = "Press on the microphone and speak into it so it can suggest icebreaker topics",
     disabled = True,
     bgcolor = "lightblue",
     border_color = "blue",
@@ -9,41 +10,48 @@ instruction_text = ft.TextField(
     multiline = True,
     min_lines = 2,
     max_lines = 4,
-    expand = False
+    expand = False,
 )
 
 mic_button = ft.Container(
     content = ft.Image(
-        src = "https://img.freepik.com/premium-vector/vector-microphone-icon-blue-microphone-button-flat-design-record-button-isolated_118339-1767.jpg",
+        src = "https://cdn-icons-png.flaticon.com/512/8863/8863272.png",
         width = 700,
         height = 700
     ),
     tooltip = "Speak",
-    on_click = lambda e: print("Mic clicked!"),
+    on_click = buttonClick.micButtonClick,
+    width = 700,
+    height = 700,
+    border_radius = 50,  #circle hitbox
     alignment = ft.alignment.top_left,
     margin = ft.margin.only(top = 170, left = 50)
 )
 
 
-refresh_icon = ft.Container(
+cancel_icon = ft.Container(
     content = ft.Image(
-        src = "https://i.fbcd.co/products/original/27e1bd4903a0e30e8e1c9daaf21b780704be85f408a163876c439dd6459f6384.jpg", 
+        src = "https://img.icons8.com/fluent-systems-regular/512/228BE6/cancel.png", 
         width = 100,
         height = 100
     ),
-    tooltip = "Refresh",
-    on_click = lambda e: print("Refresh clicked!"),
+    tooltip = "Cancel",
+    on_click = buttonClick.cancelButtonClick,
+    width = 100,
+    height = 100,
+    border_radius = 50,  #circle hitbox
+    clip_behavior = ft.ClipBehavior.HARD_EDGE,  # ensure it's clipped
     alignment = ft.alignment.top_right,
     margin = ft.margin.only(top = 20, left = 230) 
 )
 
-mic_refresh_group = ft.Stack([
+mic_cancel_group = ft.Stack([
     mic_button,
-    refresh_icon
+    cancel_icon
 ], width = 320, height = 320)  
 
 left_column = ft.Column(
-    controls = [mic_refresh_group],
+    controls = [mic_cancel_group],
     alignment = "start",
     horizontal_alignment = "start",
     expand = True
