aa996301fd107bb5a579b52f1be777e8f5dbfce9
Prince <144636788+PrinceZ-8@users.noreply.github.com>
2025-09-21 01:08:37 -0700

    buttonClick.py

--- buttonClick.py
+++ buttonClick.py
@@ -1,16 +1,19 @@
 import flet as ft
-import homeScreen
+#import homeScreen
 import audio
 import request
+
+icebreaker_questions="Model is Processing ..."
 
 def micButtonClick(e):
     print("Mic clicked!")
     audio.record_audio()
 
 def cancelButtonClick(e):
+    global icebreaker_questions
     print("Cancelling...")
     audio.stop_audio()
     text = audio.audio_to_text()
     print(text)
-    questions = request.get_icebreakers(text)
-    print(questions)
+    icebreaker_questions = request.get_icebreakers(text)
+    print(icebreaker_questions)
