c75b2af6fd074eee54852c4a117690fe7db1e1a1
Prince <144636788+PrinceZ-8@users.noreply.github.com>
2025-09-20 21:31:08 -0700

    request.py

--- Backend/request.py
+++ Backend/request.py
@@ -1,37 +1,42 @@
 from google import genai
 from supermemory import Supermemory
+import audio
 #import os
-from audio import text
 
-#Google Gemini Request --------------------------------------------------------------------------------
-UserID = "User: "
-prefix_question = "Form exactly 5 Icebreaker questions, based the following user conversation, to continue the conversation."
-content = prefix_question + text
+
 GEMINI_API_KEY = ""
+SUPER_MEMORY_KEY = ""
 
-#client = genai.Client(api_key=os.getenv(“GEMINI_API_KEY”))
-client = genai.Client(api_key=GEMINI_API_KEY)
+#Google Gemini Request ----------------------------------------------
+def get_icebreakers():
+    # sample_text = "Hello my name is prince, I am majoring in computer science, I take computer science"
+    prefix_question = "Form exactly 5 Icebreaker questions, based the following user conversation, to continue the conversation."
+    content = prefix_question + audio.text
 
-response = client.models.generate_content(
-	model="gemini-2.5-flash",
-	contents=content
-)
-print(response.text)
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    response = client.models.generate_content(
+	    model="gemini-2.5-flash",
+	    contents=content
+    )
+    generated_text = response.text
 
 
 
-#Supermemory Storage -----------------------------------------------------------------------------------
+    #Supermemory Storage -------------------------------------------
 
-client = Supermemory(
-    api_key="",
-    base_url="https://api.supermemory.ai/"
-)
-training_info = content + response.text
+    sm_client = Supermemory(api_key=SUPER_MEMORY_KEY, base_url="https://api.supermemory.ai/")
+    training_info = content + generated_text
+    store_response = sm_client.memories.add(content=training_info)
 
-# memory_data = {
-#     "id": "Prince",  # you can create a unique ID for this entry
-#     "content": training_info
-# }
+    return generated_text
 
-store_response = client.memories.add(content=training_info)
-print("Stored in Supermemory:", store_response)
+
+questions = get_icebreakers()
+print(questions)
+
+
+"""
+#client = genai.Client(api_key=os.getenv(GEMINI_API_KEY))
+#api_key=os.getenv(SUPER_MEMORY_KEY)
+"""
