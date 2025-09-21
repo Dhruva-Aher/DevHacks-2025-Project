b2d888c1f9505fea0a6ddbcf4a61595012e1dc9f
Prince <144636788+PrinceZ-8@users.noreply.github.com>
2025-09-20 23:08:26 -0700

    buttonClick.py

--- buttonClick.py
+++ buttonClick.py
@@ -1,8 +1,16 @@
 import flet as ft
 import homeScreen
+import audio
+import request
 
 def micButtonClick(e):
     print("Mic clicked!")
+    audio.record_audio()
 
 def cancelButtonClick(e):
     print("Cancelling...")
+    audio.stop_audio()
+    text = audio.audio_to_text()
+    print(text)
+    questions = request.get_icebreakers(text)
+    print(questions)
