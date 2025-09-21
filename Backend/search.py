6e3725599492d464eec9e0f943fd4c7a8832cca3
ethanngyen <ahn008huy@gmail.com>
2025-09-21 11:50:48 -0700

    Update search.py

--- Backend/search.py
+++ Backend/search.py
@@ -1,20 +1,23 @@
 #from supermemory import AsyncSupermemory
 from supermemory import Supermemory
 import asyncio
-
+from dotenv import load_dotenv
 #import request
-#import os
+import os
 
 # client = AsyncSupermemory(
 #     api_key="sm_opamYwWLBTUQt52p3eQLTs_PqTVisjbloPVfuqYMbeVRkJomfBQmMKyspkqWWprSIonTuRYHRVagLoadJKOBdyE",
 # )
 
+# Load environment variables
+load_dotenv()
+
 client = Supermemory(
-    api_key="",
-    base_url="https://api.supermemory.ai/"
+    api_key = os.environ.get("SUPERMEMORY_API_KEY"),
+    base_url = "https://api.supermemory.ai/"
 )
 
 search_response = client.search.execute(
-     q="What do I like to do?",
+     q = "What do I like to do?",
 )
 print(search_response.results)
